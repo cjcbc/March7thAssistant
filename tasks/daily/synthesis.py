8ea3785873b4403d593cfd604ca375a0fe8a1617
cjcbc <148549778+cjcbc@users.noreply.github.com>
2025-06-29 13:57:35 +0800

    Update synthesis.py
    添加升级遗器功能

--- tasks/daily/synthesis.py
+++ tasks/daily/synthesis.py
@@ -3,8 +3,46 @@ from module.automation import auto
 from module.logger import log
 import time
 
+import pygetwindow as gw
+
 
 class Synthesis:
+    @staticmethod
+    def upgrade_relic():
+        try:
+            log.hr("准备升级遗器", 2)
+            screen.change_to('bag_relicset')
+            # 筛选规则
+            if auto.click_element("./assets/images/share/synthesis/filter.png", "image", 0.9, max_retries=10):
+                # 等待筛选界面弹出
+                time.sleep(1)
+                if auto.click_element("assets/images/zh_CN/relicset/statement.png","image", 0.9, max_retries=10):
+                    if auto.click_element("./assets/images/zh_CN/relicset/not_max.png","image", 0.9, max_retries=10):
+                        # 获取窗口坐标
+                        window = gw.getWindowsWithTitle("崩坏：星穹铁道")[0]
+                        if not window:
+                            raise Exception("未找到崩坏：星穹铁道窗口")
+                        # 获取窗口位置和大小
+                        left, top = window.left, window.top
+                        left = left + 136
+                        top = top + 213
+                        right = left + 235 - 136
+                        bottom = top + 318 - 213
+                        if auto.click_element_with_pos(((left,top),(right, bottom))):
+                            for _ in range(10):
+                                if auto.click_element("./assets/images/zh_CN/relicset/upgrade.png", "image", 0.9, max_retries=10):
+                                    if auto.click_element("./assets/images/zh_CN/relicset/auto_in.png", "image", 0.9, max_retries=10):
+                                        if auto.click_element("./assets/images/zh_CN/relicset/upgrade.png", "image", 0.9, max_retries=10):
+                                            time.sleep(3)
+                                            if auto.click_element("./assets/images/zh_CN/base/click_close.png", "image", 0.8, max_retries=10):
+                                                log.info("升级遗器完成")
+                                                screen.change_to('main')
+                                                return True                                       
+            log.error("升级遗器失败")
+        except Exception as e:
+            log.error(f"升级遗器失败: {e}")
+        return False
+
     @staticmethod
     def consumables():
         try:
